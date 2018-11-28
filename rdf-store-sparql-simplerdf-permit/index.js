2ca033bc9e3621c25be08289bec0d3415a4baa78
Michael Rauch <git.mra@miranet.ch>
2018-11-28 18:19:12 +0100

    include agency in construct

--- rdf-store-sparql-simplerdf-permit/index.js
+++ rdf-store-sparql-simplerdf-permit/index.js
@@ -27,14 +27,41 @@ function objVal(any) {
 const store = new SparqlStore('https://permits.zazukoians.org/query')
 
 // const query = `DESCRIBE <https://permits.zazukoians.org/permits/1>`
+
 const query = `BASE <http://permits.integ.ld.admin.ch/>
 PREFIX schema: <http://schema.org/>
 PREFIX bdb: <http://permits.ld.admin.ch/ns#>
-CONSTRUCT {
-  ?permit ?pp ?po  
+CONSTRUCT {  
+  ?permit a schema:GovernmentPermit .
+  ?permit bdb:permitLevel ?permitLevel .
+  ?permit schema:permitAudience ?permitAudience .
+  ?permit bdb:status ?status .
+  ?permit schema:contactPoint ?contactPoint .
+  ?permit bdb:legalFoundation ?legalFoundation .
+  ?permit schema:dateModified ?dateModified .
+  ?permit schema:description ?description .
+  ?permit schema:name ?name .
+  ?permit schema:url ?url .
+  ?permit bdb:agency ?agency .
+  
+  ?agency ?ap ?ao .
 }
-WHERE {
-  ?permit ?pp ?po .
+WHERE {          
+  ?permit bdb:permitLevel ?permitLevel .  
+  OPTIONAL { ?permit schema:permitAudience ?permitAudience }
+  OPTIONAL { ?permit bdb:status ?status }  
+  OPTIONAL { ?permit schema:contactPoint ?contactPoint }
+  OPTIONAL { ?permit bdb:legalFoundation ?legalFoundation }
+  OPTIONAL { ?permit schema:dateModified ?dateModified }
+  OPTIONAL { ?permit schema:description ?description }
+  OPTIONAL { ?permit schema:name ?name }      	
+  OPTIONAL { ?permit schema:url ?url }
+  
+  OPTIONAL {
+    ?permit bdb:agency ?agency .
+    ?agency ?ap ?ao .
+  }
+  
   {
     SELECT DISTINCT ?permit WHERE {
       VALUES ?permitLevel {
@@ -58,6 +85,7 @@ WHERE {
 
 const context = {
     'dateModified': 'http://schema.org/dateModified',
+    'agency': 'http://permits.ld.admin.ch/ns#agency',
 }
 
 const stream = store.construct(query)
@@ -100,6 +128,15 @@ dataset.import(stream).then(() => {
         console.log(`url:            ${url}`)
         console.log(`legalFound.:    ${legalFoundation}`)
         console.log(`dateModified:   ${permit.dateModified}`)
+
+        if (!! permit.agency) {
+            let agencyAsNamedNode = permit.agency._core.iri
+            const agencyName = objVal(findQuadByLanguage(dataset, agencyAsNamedNode, rdf.namedNode('http://schema.org/name'), langPrios))
+            const agencyAcronym = objVal(findQuadByLanguage(dataset, agencyAsNamedNode, rdf.namedNode('http://permits.ld.admin.ch/ns#acronym'), langPrios))
+            console.log(`agency-name:    ${agencyName}`)
+            console.log(`agency-acronym: ${agencyAcronym}`)
+        }
+
         console.log('----------------------------------------------')
 
     })    
